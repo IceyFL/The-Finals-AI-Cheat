0d8e9c30006124926bd6c629fee6b0da05aa5257
owenlol2132 <owenthetrueman@gmail.com>
2024-02-09 20:49:46 +0000

    stuff

---
+++ Dark The Finals/dist/Properties.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace dist {
+    class Properties {
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct OBJECT_ATTRIBUTES : IDisposable {
+            public int Length;
+            public IntPtr RootDirectory;
+            private IntPtr objectName;
+            public uint Attributes;
+            public IntPtr SecurityDescriptor;
+            public IntPtr SecurityQualityOfService;
+
+            public OBJECT_ATTRIBUTES(string name, uint attributes) {
+                Length = 0;
+                RootDirectory = IntPtr.Zero;
+                objectName = IntPtr.Zero;
+                Attributes = attributes;
+                SecurityDescriptor = IntPtr.Zero;
+                SecurityQualityOfService = IntPtr.Zero;
+
+                Length = Marshal.SizeOf(this);
+                objectName = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Properties.UNICODE_STRING)));
+                WinAPI.RtlInitUnicodeString(objectName, name);
+            }
+
+            public void Dispose() {
+                if (objectName != IntPtr.Zero) {
+                    Marshal.FreeHGlobal(objectName);
+                    objectName = IntPtr.Zero;
+                }
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct UNICODE_STRING {
+            public ushort Length;
+            public ushort MaximumLength;
+            public IntPtr Buffer;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 0)]
+        public struct IO_STATUS_BLOCK {
+            public uint Status;
+            public IntPtr Information;
+        }
+    }
+}
---
+++ Dark The Finals/dist/WinAPI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace dist {
+    class WinAPI {
+        [DllImport("ntdll.dll")]
+        public static extern void RtlInitUnicodeString(IntPtr DestinationString, [MarshalAs(UnmanagedType.LPWStr)] string SourceString);
+
+        [DllImport("ntdll.dll", ExactSpelling = true, SetLastError = true)]
+        public static extern int NtCreateFile(
+            out IntPtr handle,
+            int access,
+            ref Properties.OBJECT_ATTRIBUTES objectAttributes,
+            ref Properties.IO_STATUS_BLOCK ioStatus,
+            IntPtr allocSize,
+            uint fileAttributes,
+            int share,
+            uint createDisposition,
+            uint createOptions,
+            IntPtr eaBuffer,
+            uint eaLength);
+
+        [DllImport("ntdll.dll", ExactSpelling = true, SetLastError = true)]
+        public static extern int NtDeviceIoControlFile(
+            IntPtr fileHandle,
+            IntPtr eventHandle,
+            IntPtr apcRoutine,
+            IntPtr ApcContext,
+            ref Properties.IO_STATUS_BLOCK ioStatus,
+            uint controlCode,
+            ref Struct.MOUSE_IO inputBuffer,
+            int inputBufferLength,
+            IntPtr outputBuffer,
+            int outputBufferLength
+        );
+
+        [DllImport("ntdll.dll")]
+        public static extern int ZwClose(IntPtr h);
+    }
+}
